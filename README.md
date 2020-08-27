4a5ff87d07dd281cd77d11d175c50baef0734389
Richard Yu <vipzhicheng@gmail.com>
2020-08-27 21:22:28 +0800

    init: semo-plugin-repl-hint

---
+++ README.md
@@ -0,0 +1,21 @@
+# semo-plugin-repl-hint
+
+A Semo plugin to provide ability to get dev hints from devhints.io
+
+## Installation & Usage
+
+```
+npm i -g @semo/cli semo-plugin-repl-hint
+semo repl --hook
+>>> .hint react
+>>> .hint vue --copy
+>>> .hint lodash -C
+```
+
+## Why `--copy` / `-C`
+
+Because devhints.io give long hints to us, if you want to save to other place, it's hard to copy from terminal.
+
+## Licence
+
+MIT
---
+++ src/hooks/index.ts
@@ -0,0 +1,56 @@
+import { convertUrlToMarkdown, convertMarkdownToFile } from 'semo-plugin-read'
+import marked from 'marked'
+import TerminalRenderer from 'marked-terminal'
+marked.setOptions({
+  renderer: new TerminalRenderer()
+})
+
+export = (Utils) => {
+  return {
+    hook_repl_command: new Utils.Hook('semo', () => {
+      return {
+        hint: {
+          help: 'Get dev hints from devhints.io',
+          async action(input) {
+            if (!input) {
+              Utils.warn('keyword is required')
+            } else {
+              // @ts-ignore
+              this.clearBufferedCommand()
+              let opts = Utils.yParser(input)
+              let keyword = opts._.join(' ')
+
+              const converted = await convertUrlToMarkdown({
+                url: `https://devhints.io/${keyword}`
+              })
+
+              const { markdown, title } = converted
+
+              if (opts.copy || opts.C) {
+                await convertMarkdownToFile({
+                  format: 'clipboard',
+                  markdown,
+                  title,
+                  converted,
+                  argv: {
+                    url: `https://devhints.io/${keyword}`
+                  }
+                })
+              }
+
+              Utils.consoleReader(marked(markdown), {
+                plugin: 'semo-plugin-repl-hint',
+                identifier: input
+              })
+
+
+               // @ts-ignore
+              this.displayPrompt()
+            }
+          }
+
+        }
+      }
+    })
+  }
+}
